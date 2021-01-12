e4c24b0cc7fe81bfc0d87d206d266330626c4750
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-12 06:28:26 -0300

    Add Petitions struct

---
+++ WhitehousePetitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhitehousePetitions
+//
+//  Created by Giovanna Pezzini on 12/01/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}

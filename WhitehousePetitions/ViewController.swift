8017e7ba50b967f650b5bba02292e7ed59fd5114
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-12 06:06:49 -0300

    Add tableView methods

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        navigationController?.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 0)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Description"
+        return cell
     }
 }
 
